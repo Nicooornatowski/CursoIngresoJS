ba31c109396f55782a2131f0090d89a8e5b37ef1
Nicooornatowski <86834938+Nicooornatowski@users.noreply.github.com>
2021-07-06 21:14:22 -0300

    Add files via upload
    primer commit

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,8 +4,15 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	var mensajeSalida
+	//nombreIngresado=txtIdNombre.value;
+	// 1er error nombreIngresado=document.getElementById('txtIdNombre');
+	// 2do error nombreIngresado=document.getElementById('txtidNombre').value;
+	nombreIngresado=document.getElementById('txtIdNombre').value;
+
+	mensajeSalida="su nombre es: "+nombreIngresado
+
+	alert(mensajeSalida);
 
 }
 
